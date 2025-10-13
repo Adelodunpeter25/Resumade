5be203c9b0fc3ed71ad32772ee65062b9df22c5f
Adelodunpeter25 <adelodunpeter69@gmail.com>
2025-10-13 18:02:01 +0100

    fixed import errors

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -10,3 +10,10 @@ class TokenData(BaseModel):
 class LoginRequest(BaseModel):
     email: EmailStr
     password: str
+
+class ForgotPasswordRequest(BaseModel):
+    email: EmailStr
+
+class ResetPasswordRequest(BaseModel):
+    token: str
+    new_password: str
